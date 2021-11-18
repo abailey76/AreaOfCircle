1728f35d07e80ba99634b29262fcff7d2a002136
Adam Bailey <abailey76@duck.com>
2021-11-18 17:36:35 -0600

    post group work and pre instructor walkthrough

--- AreaOfCircle/Program.cs
+++ AreaOfCircle/Program.cs
@@ -10,25 +10,11 @@ namespace AreaOfCircle
 
             double radius = Convert.ToDouble(Console.ReadLine());
 
-            double area = Math.PI * radius * radius;
-
-            double circumference = 2 * Math.PI * radius;
-
-            double diameter = 2 * radius;
+            double area = Math.PI * (radius * radius);
 
             Console.WriteLine("Area of circle: " + area);
 
-            Console.WriteLine("Circumference of circle: " + circumference);
 
-            Console.WriteLine("Diameter of circle: " + diameter);
-
-            Console.WriteLine("\nWhat is the average mpg of your car?");
-
-            double avgMpg = Convert.ToDouble(Console.ReadLine());
-
-            double roadTripGas = circumference / avgMpg;
-
-            Console.WriteLine("You would burn " + roadTripGas + " gallons of gas, traveling around this circle.");
         }
     }
 } 
